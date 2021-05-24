1478abe9068377193a982cfd172cdf8a394205da
doksiri <payengrepublica@gmail.com>
2021-05-24 21:31:54 +0530

    first commit

---
+++ Tower-Siege-2-Template-main/Block.js
@@ -0,0 +1,31 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.visibility = 255;
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos= this.body.position;
+      push();
+      if(this.body.speed<3){
+
+      rectMode(CENTER);
+      rect(pos.x,pos.y,this.width, this.height);
+      pop();
+    }
+      else {
+        push();
+        World.remove(world, this.body);
+        this.visibility = this.visibility - 1;
+        tint(255, this.visibility);
+        pop();
+    }
+      
+    }
+}
